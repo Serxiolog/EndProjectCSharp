762cf0f087d03669ed46962106e00a351635a6af
Сергей Козырев <s.kozyrev_sk@mail.ru>
2022-05-05 15:17:20 +0400

    Difficulity

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
     public List<GameObject> RightCubes = new List<GameObject>();
     public List<GameObject> LeftCubes = new List<GameObject>();
     public List<GameObject> StandartCubes = new List<GameObject>();
+    public List<GameObject> DifficulityImages = new List<GameObject>();
     private List<List<GameObject>> RL = new List<List<GameObject>>();
     private List<float[]> RB= new List<float[]>();
     private float[] CoordsX = new float[6] {-0.7f, -0.45f, -0.2f, 0.2f, 0.45f, 0.7f };
@@ -15,18 +17,21 @@ public class GameScript : MonoBehaviour
     private float[] CoordsXBlue = new float[3] { -0.7f, -0.45f, -0.2f };
     private float[] CoordsY = new float[2] { 1f, 0.75f };
     private float StartZ = 10;
+    public int Score;
     public float GameTime;
     public bool HardMode;
     private System.Random rnd = new System.Random();
     private List<GameObject> Moving = new List<GameObject>();
     public float Speed = 1;
     public GameObject Walls;
+    public Text Timer;
 
     public float TimeGeneration = 1;
     private float TimePreGeneration = 0;
 
     void Start()
     {
+        Score = 0;
         GameTime = 0;
         HardMode = false;
         RL.Add(RightCubes);
@@ -46,10 +51,10 @@ public class GameScript : MonoBehaviour
                 if (HardMode)
                 {
                     int a, b, c, d; //a:CoordY; b:CoordX; c:StandartCube; d:Side
-                    a = rnd.Next(0, 1);
-                    b = rnd.Next(0, 5);
-                    c = rnd.Next(0, 5);
-                    d = rnd.Next(0, 1);
+                    a = rnd.Next(0, 2);
+                    b = rnd.Next(0, 6);
+                    c = rnd.Next(0, 6);
+                    d = rnd.Next(0, 2);
                     var _Cube = Instantiate(RL[d][c], new Vector3(RB[d][b], CoordsY[a], StartZ), Quaternion.identity);
                     Moving.Add(_Cube);
                 }
@@ -73,7 +78,7 @@ public class GameScript : MonoBehaviour
         {
             Walls.SetActive(false);
         }
-        else
+        else if (Moving.Count == 0)
         {
             Walls.SetActive(true);
         }
@@ -98,5 +103,36 @@ public class GameScript : MonoBehaviour
     public void GameStart()
     {
         GameTime = 100;
+    }
+
+    public void TimeChange()
+    {
+        TimeGeneration += 1;
+        TimeGeneration %= 5;
+        if (TimeGeneration == 0)
+            TimeGeneration = 1;
+        Timer.text = $"{TimeGeneration} sec";
+        
+    }
+
+    public void DifficulityChange()
+    {
+        if (HardMode)
+        {
+            HardMode = false;
+            DifficulityImages[0].SetActive(true);
+            DifficulityImages[1].SetActive(false);
+        }
+        else
+        {
+            HardMode = true;
+            DifficulityImages[0].SetActive(false);
+            DifficulityImages[1].SetActive(true);
+        }
+    }
+
+    public void Checker()
+    {
+        Debug.Log("Changed");
     }
 }
